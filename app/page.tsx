5b0b7359c150a4682eb262f91feb0dd5a60cff1e
Samuel <smenxah@gmail.com>
2026-02-10 11:35:33 +0000

    feat: implement chat component with message handling and input functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { useState } from "react";
+
 export default function Chat() {
-  return <main>Hello, world!</main>;
+  const [input, setInput] = useState("");
+  const { messages, sendMessage } = useChat();
+
+  return (
+    <div className="">
+      <div className="">
+        {messages.map((message, i) => (
+          <div key={message.id} className="">
+            <div className="">
+              <div className="">{message.role}</div>
+              {message.parts.map((part) => {
+                switch (part.type) {
+                  case "text":
+                    return <p>{part.text}</p>;
+                }
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage({ text: input });
+          setInput("");
+        }}
+      >
+        <input
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+          placeholder="Say something..."
+          className=""
+        />
+      </form>
+    </div>
+  );
 }
